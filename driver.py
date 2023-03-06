62a446c40c4a0a0ce3e6e5ecc030cfc15288093e
atul107 <kr.atul.atk@gmail.com>
2023-03-06 11:07:09 +0530

    add driver

---
+++ driver.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from seleniumwire.utils import decode as sw_decode
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Driver:
+    #Abstract base class for browser drivers
+    def __init__(self, args):
+        self.browser = None
+        self.setup(args)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def setup(self, args):
+        #Abstract method to set up the browser
+        pass
+
+    def close(self):
+        #Quit the browser
+        self.browser.quit()
+
+class ChromeDriver(Driver):
+    #Chrome browser driver
+
+    def __init__(self, args):
+        super().__init__(args)
+
+    def setup(self, args):
+        #Set up the Chrome browser
+        opts = webdriver.ChromeOptions()
+        user_agent = args['user_agent']
+        opts.add_argument(f'user-agent={user_agent}')
+        for arg in args['additional_args']:
+            opts.add_argument(arg)
+        self.browser = webdriver.Chrome(desired_capabilities=opts.to_capabilities())
